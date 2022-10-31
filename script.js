ca4c492f5670e8ed8f281620e4f9643d80aa6221
OliveiraJPE <peoj2009@gmail.com>
2022-10-31 08:41:12 -0300

    Figura menor

---
+++ script.js
@@ -0,0 +1,9 @@
+    
+    var h = window.document.querySelector("h1#paragr");
+    
+    var largura = window.innerWidth; //window.screen.width - valor total da tela;
+    //h.innerHTML=`${largura}`;
+   if(largura<600){
+    //h.innerHTML=`${largura}`;
+    h.innerHTML=`<h1 id="paragr" style="font-size:0.5em;">Coisas de fissurada</h1>`
+    }
